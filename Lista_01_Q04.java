e07e5280803b3c3c417f083bf6271e2aa727da3a
Jorge Luis <luisquirinobraga@gmail.com>
2026-09-13 00:20:34 -0300

    Update Lista_01_Q04.java
    adicionado questoes

--- Lista_01_Q04.java
+++ Lista_01_Q04.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] numeros = new int[5];
+void main () {
+    int[] numeros = new int[5];
+    Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Digite o " + (i + 1) + "º número (entre 1 e 30): ");
-            int num = sc.nextInt();
-            
-            if (num >= 1 && num <= 30) {
-                numeros[i] = num;
-            } else {
-                System.out.println("Número inválido! Digite um valor entre 1 e 30.");
-                i--; // Decrementa o contador para repetir a leitura desta posição
-            }
+    for (int i = 0; i < 5; i++) {
+        System.out.println("Digite o " + (i + 1) + "º número (entre 1 e 30):");
+        int num = scanner.nextInt();
+        
+        if (num >= 1 && num <= 30) {
+            numeros[i] = num;
         }
-
-        System.out.println("\nGráfico de Barras:");
-        for (int num : numeros) {
-            for (int j = 0; j < num; j++) {
-                System.out.print("*");
-            }
-            System.out.println(); // Pula para a próxima linha após imprimir os asteriscos do número
+        if (num < 1 || num > 30) {
+            System.out.println("Número inválido! Digite um valor entre 1 e 30.");
+            i--;
         }
+    }
 
-        sc.close();
+    System.out.println("Gráfico de Barras:");
+    for (int num : numeros) {
+        for (int j = 0; j < num; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
